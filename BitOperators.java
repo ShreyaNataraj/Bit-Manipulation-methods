d82d0f9ea0f4eec5b5d05e2122f4099a54a95e98
Shreya N <nshreya094@gmail.com>
2024-01-27 23:16:10 +0530

    Bit Basic Operation

---
+++ BitOperators.java
@@ -0,0 +1,10 @@
+public class BitOperators{
+  public static void main(String[] args) {
+    System.out.println(5&6);//AND OPERATION
+    System.out.println(5|6);//OR OPERATION
+    System.out.println(5^6);//XOR OPERATION
+    System.out.println(~5);//ONE'S COMPLEMENT
+    System.out.println(5<<2);//LEFT SHIFT
+    System.out.println(6>>1);//RIGH SHIFT
+  }
+}
